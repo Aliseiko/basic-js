f834b4b6c5272117e1f9b7dada53127219018ef8
Aliseiko <aliseiko.dev@gmail.com>
2022-02-27 19:03:55 +0300

    feat: Vigenere cipher done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -23,7 +23,10 @@ class VigenereCipheringMachine {
   constructor(machineIsDirect = true) {
     this.machineIsDirect = machineIsDirect;
   }
+
   encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
     let result = '',
         messageUp = message.toUpperCase(),
         resultKey, encriptKey = '';
@@ -48,11 +51,40 @@ class VigenereCipheringMachine {
       }
     }
 
-    return result
+    let reversedResult = result.split('').reverse().join('')
+
+    return (this.machineIsDirect) ? result : reversedResult
   }
   decrypt(encryptedMessage, key) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (encryptedMessage === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+    let result = '',
+        encryptedMessageUp = encryptedMessage.toUpperCase(),
+        resultKey, encriptKey = '';
+    let count = 0;
+    for (let i = 0; i < encryptedMessageUp.length; i++) {
+      if (encryptedMessageUp.charCodeAt(i) >= 65 && encryptedMessageUp.charCodeAt(i) <= 90) {
+        encriptKey += key[count % key.length].toUpperCase();
+        count++;
+      } else {
+        encriptKey += ' ';
+      }
+      console.log(encriptKey)
+    }
+
+    for (let i = 0; i < encryptedMessageUp.length; i++) {
+      if (encryptedMessageUp.charCodeAt(i) >= 65 && encryptedMessageUp.charCodeAt(i) <= 90) {
+        resultKey = String.fromCharCode(((encryptedMessageUp.charCodeAt(i) - 65) - (encriptKey.charCodeAt(i) - 65) + 26) % 26 + 65);
+        result += resultKey;
+      } else {
+        result += encryptedMessage[i];
+      }
+    }
+
+    let reversedResult = result.split('').reverse().join('')
+
+    return (this.machineIsDirect) ? result : reversedResult
+
   }
 }
 
